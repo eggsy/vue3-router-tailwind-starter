e239be78ae1ae7a784a0906a123072f15c6aa26b
Abdulbaki Dursun <eggsydev@gmail.com>
2022-09-13 02:25:04 +0300

    feat: use pages plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-  plugins: [vue()],
+  plugins: [Vue(), Pages({ dirs: ["src/views"] })],
 });
