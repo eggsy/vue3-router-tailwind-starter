a3807476de8471e14f6948b7ac0b6a26c5181b9d
Abdulbaki Dursun <eggsydev@gmail.com>
2023-10-29 22:50:36 +0300

    style: update index page style

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="text-4xl">Hello World</div>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 class="text-4xl">Hello World</h1>
+  </div>
 </template>
